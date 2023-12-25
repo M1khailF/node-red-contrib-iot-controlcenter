336754f78f2433bd903ac35c816747b1b03b0390
Mike <122553871+M1khailF@users.noreply.github.com>
2023-12-25 16:37:13 +0300

    v1.0.6

--- IOT_connectNode.js
+++ IOT_connectNode.js
@@ -1,18 +1,35 @@
 module.exports = function(RED) {
     const express = require('express');
     const app = express();
-    //const request = require('request');
+    const request = require('request');
     const PORT = 17380
     var postJson = ""
+    const { random } = require('lodash');
+    // const my_random = require('lodash.random');
     ///var getJson = ""
+    app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
 
     app.post('/platform_set', (req, res) => {
-        res.send(req.body + + "ds");
+        const message = req.body.message;
+
+        // Display the received message on the HTTP page
+        res.send(`<h1>Received Message: ${message}</h1>`);
+        // const jsonData = req.body;
+        // res.setHeader('Content-Type', 'application/json');
+        // res.send(JSON.stringify(jsonData));
+        // console.log(JSON.stringify(jsonData));
     });
 
     app.get('/platform_set', (req, res) => {
+        const message = req.body.message;
+
+        // Display the received message on the HTTP page
         res.send(postJson);
+        // const jsonData = req.body;
+        // res.setHeader('Content-Type', 'application/json');
+        // res.send(JSON.stringify(jsonData));
+        // console.log(JSON.stringify(jsonData));
     });
 
     app.listen(PORT, () => {
@@ -20,17 +37,24 @@ module.exports = function(RED) {
         console.log(currentDate.getDate() + " " + currentDate.toLocaleString('default', { month: 'short' }) + " " + ("0" + currentDate.getHours()).slice(-2) + ":" + ("0" + currentDate.getMinutes()).slice(-2) + ":" + ("0" + currentDate.getSeconds()).slice(-2) + ' - [info] Applied Robotics preset');
     });
 
+    function displayMessage(message, ms, ns) {
+        setTimeout(() => {
+            postJson = message;
+        }, ms + ns);
+      }
+    
     function IOT_connectNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
         node.name = config.name;
         node.ip = config.ip;
    
-        this.on('input', function(msg) {
+        this.on('input', async function(msg) {
             var jsonMSG = JSON.stringify(msg.payload);
-            postJson = msg.payload
-            node.send(postJson)
-            //node.status({fill:"green", shape:"dot", text:"connected"});
+            const randomNumber = random(0, 100);  
+            node.send(postJson);
+
+            displayMessage(msg.payload, random(0, 100), random(0, 10));
         });
     }
 
